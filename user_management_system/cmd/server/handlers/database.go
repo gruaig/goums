c51926f1371c46ce2a9f8f466727a712c581c4d2
sean.monaghan <sean.monaghan@alvarfinancial.com>
2024-03-31 22:31:23 +0200

    SQLGen

--- user_management_system/cmd/server/handlers/database.go
+++ user_management_system/cmd/server/handlers/database.go
@@ -1,25 +1,23 @@
-package main
+package handlers
 
 import (
-	"context"
 	"database/sql"
 	"log"
-
-	"github.com/gruaig/goums/users"
-	_ "github.com/lib/pq"
 )
 
-func main() {
-
+func pingTest() {
 	connecStr := "postgres://postgres:password@localhost:5432?goums?sslmode=disable"
 	db, err := sql.Open("postgres", connecStr)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	defer db.Close()
 
-	ctx := context.Background()
-	queries := users.New(db)
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 }
